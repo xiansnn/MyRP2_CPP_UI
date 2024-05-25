5ddfe909ae9164835ceddb4b354e5d2d5fe5b16d
Chris <42316927+xiansnn@users.noreply.github.com>
2024-05-25 15:21:29 +0200

    add text widget on serial monitor

---
+++ sw/widgets/widget_print_console.cpp
@@ -0,0 +1,10 @@
+#include "widget_print_console.h"
+
+WTextSerialMonitor::WTextSerialMonitor()
+: UIWidget(nullptr,128,8,0,0,false)
+{
+}
+
+WTextSerialMonitor::~WTextSerialMonitor()
+{
+}
---
+++ sw/widgets/widget_print_console.h
@@ -0,0 +1,17 @@
+#if !defined(WIDGET_PRINT_CONSOLE_H)
+#define WIDGET_PRINT_CONSOLE_H
+
+#include "ui_core.h"
+
+class WTextSerialMonitor : public UIWidget
+{
+private:
+    /* data */
+public:
+    WTextSerialMonitor();
+    ~WTextSerialMonitor();
+};
+
+
+
+#endif // WIDGET_PRINT_CONSOLE_H
